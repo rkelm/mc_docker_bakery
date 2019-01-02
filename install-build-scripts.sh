48b067038f1953a29e14e0368bf88702f8adcf8e
Robert Kelm <git@kelm.net>
2019-01-02 10:04:35 +0100

    Basic vagrant setup and first provision script.

---
+++ install-build-scripts.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Script to download and install all build scripts for building
+# the minecraft server containers images.
+
+# Vanilla MC Container.
+git clone https://github.com/rkelm/docker_image_mc_vanilla.git
+
+
