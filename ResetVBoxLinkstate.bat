2e0ddd922cd27738200f700702fac97e84134c0a
Robert Kelm <git@kelm.net>
2021-09-11 16:47:19 +0200

    forward MC default port

--- ResetVBoxLinkstate.bat
+++ ResetVBoxLinkstate.bat
@@ -1,4 +1,5 @@
-REM Batch file to fix network connectivity of vagrant/VBox guest after host standby/hybernate. 
+REM Batch file to fix network connectivity of vagrant/VBox guest after host standby/hybernate.
+REM Name of vm needs to be edited to match the installed vms name. 
 VBoxManage controlvm mc_docker_bakery_default_1546622094946_67594 setlinkstate1 off
 VBoxManage controlvm mc_docker_bakery_default_1546622094946_67594 setlinkstate1 on
 
